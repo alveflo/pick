b4810680dcdc3306188d34e7d8db6ff076a61be0
Victor Alveflo <victor.alveflo@gmail.com>
2014-10-09 20:26:02 +0000

    Added support for arrays.

--- pick/pick.js
+++ pick/pick.js
@@ -1,26 +1,58 @@
 'use strict'
 
-Object.prototype.pick = function() {
+var parseObject = function() {
+	var obj = arguments[0];
 	var ret = {};
-	for (var i in arguments) {
-		var key = arguments[i];
-		if (this.hasOwnProperty(key)) {
-			ret[key] = this[key];
+	var keys = arguments[1];
+	var filled = false;
+	for (var i in keys) {
+		var key = keys[i];
+		if (obj.hasOwnProperty(key)) {
+			ret[key] = obj[key];
+			filled = true;
 		}
 	}
+	if (filled)
+		return ret;
+	return null;
+};
+
+var parseArray = function() {
+	var ret = [];
+	var array = arguments[0];
+	var keys = arguments[1];
+	for (var i = 0; i < array.length; i++)
+	{
+		var obj = array[i];
+		var val = parseObject(obj, keys);
+		if (val != null)
+			ret.push(val);
+	}
 	return ret;
-}
+};
+
+Object.prototype.pick = function() {
+	if (this instanceof Array)
+		return parseArray(this, arguments);
+	else
+		return parseObject(this, arguments);
+};
 
 Object.prototype.into = function(obj) {
 	if (obj != null) {
-		if (typeof(obj) == 'object')
-		{
+		if (obj instanceof Array) {
+			for (var i = 0; i < obj.length; i++) {
+				obj.push()
+			}
+		}
+		else {
 			for (var key in this) {
 				if (this.hasOwnProperty(key)) {
 					obj[key] = this[key];
 				}
 			}
 		}
+
 	}
 	return obj;
-}
+};
